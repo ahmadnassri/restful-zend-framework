b8da6733350aa5baeed243bba6751bfe416e6717
Ahmad Nassri <ahmad@codeinchaos.com>
2012-02-01 18:52:57 -0500

    some cleanups and a bugfix regarding catchall action methods

--- Controller/Request/Http.php
+++ Controller/Request/Http.php
@@ -3,25 +3,6 @@ class REST_Controller_Request_Http extends Zend_Controller_Request_Http
 {
     private $_error = false;
 
-    public function __construct($action = null, $controller = null, $module = null, array $params = array())
-    {
-        if ($action) {
-            $this->setActionName($action);
-        }
-
-        if ($controller) {
-            $this->setControllerName($controller);
-        }
-
-        if ($module) {
-            $this->setModuleName($module);
-        }
-
-        if ($params) {
-            $this->setParams($params);
-        }
-    }
-
     public function setError($code, $message)
     {
         $this->_error = new stdClass;
